62e8fc0cfaee8258caca9b4c9c3acbfaa0a6fb92
Ramya Ragupathy <r.ragupathy85@gmail.com>
2019-07-29 11:20:15 +0530

    Options update

---
+++ mm-diff-status.js
@@ -0,0 +1,199 @@
+const request = require('request')
+const https = require('https')
+const moment = require('moment')
+const status = {
+  0: 'Operational',
+  1: 'Under Maintenance',
+  2: 'Degraded Performance',
+  3: 'Partial Outage',
+  4: 'Major Outage'
+}
+const args = {
+  'api_endpoint': process.env['API_ENDPOINT'],
+  'component_id': '79B2MuS7Q8XC',
+  'api_key': process.env['API_KEY']
+}
+
+exports.handler = function index (event, context, callback) {
+  var options = {
+    'method': 'GET',
+    'url': 'https://' + args['api_endpoint'] + '/api/v0/incidents',
+    'headers': {
+      'x-api-key': args['api_key'],
+      'Content-Type': 'application/json'
+    }
+  }
+  var incidentID = ''
+  var incidentTimeStamp = moment('1900-01-01T00:00:00.000Z')
+  var incidentStatus = ''
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var incidents = JSON.parse(body)
+      console.log('Checking incidents...')
+      console.log(JSON.stringify(incidents))
+      incidents.forEach(incident => {
+        if (incident.name.indexOf('Leaderboard') >= 0) {
+          if (!(incident.status === 'Resolved')) {
+            var incidentTime = moment(incident.createdAt)
+            if (incidentTimeStamp.isBefore(incidentTime)) {
+              incidentTimeStamp = incidentTime
+              incidentID = incident.incidentID
+              incidentStatus = incident.status
+            }
+          }
+        }
+      })
+      var hotDiff = ''
+      options = {
+        'method': 'PATCH',
+        'host': args['api_endpoint'],
+        'path': '/api/v0/components/' + args['component_id'],
+        'headers': {
+          'x-api-key': args['api_key'],
+          'Content-Type': 'application/json'
+        }
+      }
+      var data = {
+        'status': status[0]
+      }
+      var mmDiff = ''
+      var fetchOptions = {}
+      var incidentData = {}
+      fetchOptions.url = 'https://osm-stats-production-api.azurewebsites.net/status'
+      request(fetchOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          var mmStatus = JSON.parse(body)
+          console.log(mmStatus)
+          mmStatus.forEach(ele => {
+            if (ele.component === 'augmented diffs') {
+              mmDiff = ele.id
+            }
+          })
+          fetchOptions.url = 'http://overpass.hotosm.org/api/augmented_diff_status'
+          request(fetchOptions, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+              hotDiff = body.trim()
+              var difference = hotDiff - mmDiff
+              console.log(difference)
+              if (difference >= 5) {
+                data.status = status[2]
+                if (incidentID === '') {
+                  console.log('New incident to be created')
+                  incidentData = {
+                    'name': 'MM Leaderboard Diff Processing',
+                    'status': 'Identified',
+                    'message': 'Performance issue identified'
+                  }
+                  options = {
+                    'method': 'POST',
+                    'host': args['api_endpoint'],
+                    'path': '/api/v0/incidents/',
+                    'headers': {
+                      'x-api-key': args['api_key'],
+                      'Content-Type': 'application/json'
+                    }
+
+                  }
+                  console.log(JSON.stringify(incidentData))
+                  const req = https.request(options, (res) => {
+                    console.log('statusCode: ', res.statusCode)
+                  })
+                  req.write(JSON.stringify(incidentData))
+                  req.end()
+                } else {
+                  console.log(incidentID + ' already created')
+                  if (incidentStatus !== 'Investigating') {
+                    incidentData = {
+                      'status': 'Investigating',
+                      'message': 'Performance issue identified'
+                    }
+                    options = {
+                      'method': 'PATCH',
+                      'host': args['api_endpoint'],
+                      'path': '/api/v0/incidents/' + incidentID,
+                      'headers': {
+                        'x-api-key': args['api_key'],
+                        'Content-Type': 'application/json'
+                      }
+
+                    }
+                    console.log(options)
+                    console.log(JSON.stringify(incidentData))
+                    const req = https.request(options, (res) => {
+                      console.log('statusCode: ', res.statusCode)
+                    })
+                    req.write(JSON.stringify(incidentData))
+                    req.end()
+                  } else {
+                    console.log('Investigation status already updated')
+                  }
+                }
+                console.log('MM Diff processing is behind by ', difference)
+              } else {
+                if (incidentID !== '') {
+                  console.log(incidentID + ' already created')
+                  incidentData = {
+                    'name': 'MM Leaderboard Diff Processing',
+                    'status': 'Resolved',
+                    'message': 'Performance issue addressed'
+                  }
+                  options = {
+                    'method': 'PATCH',
+                    'host': args['api_endpoint'],
+                    'path': '/api/v0/incidents/' + incidentID,
+                    'headers': {
+                      'x-api-key': args['api_key'],
+                      'Content-Type': 'application/json'
+                    }
+
+                  }
+                  console.log(JSON.stringify(incidentData))
+                  const req = https.request(options, (res) => {
+                    console.log('statusCode: ', res.statusCode)
+                  })
+                  req.write(JSON.stringify(incidentData))
+                  req.end()
+                }
+              }
+              options = {
+                'method': 'PATCH',
+                'host': args['api_endpoint'],
+                'path': '/api/v0/components/' + args['component_id'],
+                'headers': {
+                  'x-api-key': args['api_key'],
+                  'Content-Type': 'application/json'
+                }
+              }
+              console.log(JSON.stringify(data))
+              const req = https.request(options, (res) => {
+                console.log('statusCode: ', res.statusCode)
+              })
+              req.write(JSON.stringify(data))
+              req.end()
+            }
+            options = {
+              'method': 'PATCH',
+              'host': args['api_endpoint'],
+              'path': '/api/v0/components/' + args['component_id'],
+              'headers': {
+                'x-api-key': args['api_key'],
+                'Content-Type': 'application/json'
+              }
+            }
+            const req = https.request(options, (res) => {
+              console.log('statusCode: ', res.statusCode)
+            })
+            req.write(JSON.stringify(data))
+            req.end()
+          })
+        } else {
+
+        }
+      })
+    } else {
+
+    }
+  })
+
+  callback(null)
+}
